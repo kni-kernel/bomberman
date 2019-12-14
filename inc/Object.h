77abfad080868b4bf33ea129d34fa06d066f863a
Mikołaj Marchewa <mikolaj.marchewa@gmail.com>
2019-12-14 22:53:11 +0100

    Improving some mess with Object commit (no more useless folders) .

---
+++ inc/Object.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <iostream>
+#include <vector>
+
+class Object
+{
+public:
+	Object(std::vector<double>, std::string);
+	virtual void draw() = 0;
+	virtual void update() = 0;
+protected:
+	std::vector <double> _position;
+	std::string _asset;
+};
+
+#endif //OBJECT_H
---
+++ src/Object.cpp
@@ -0,0 +1,7 @@
+#include "Object.h"
+
+Object::Object(std::vector<double> position = std::vector<double>(2, 0), std::string asset = "Default_asset")
+{
+	_position = position;
+	_asset = asset;
+}
