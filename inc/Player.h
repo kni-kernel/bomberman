7fa87f65eb1b9a05478438e62bfccead71554fd7
Mikołaj Marchewa <mikolaj.marchewa@gmail.com>
2019-12-16 20:08:46 +0100

    Player class

---
+++ inc/Player.h
@@ -0,0 +1,34 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <QtWidgets/QGraphicsPixmapItem>
+#include <QtWidgets/QGraphicsItem>
+#include <QKeyEvent>
+#include "Object.h"
+#include <utility>
+#include <vector>
+#include <iostream>
+
+class Player : public Object, public QGraphicsPixmapItem {
+public:
+	//Player(QGraphicsItem* parent, std::pair<double,double>position, std::string asset, int maxVelocity , int lifes, int availableBombs);
+	Player(
+		QGraphicsItem* parent,
+		std::pair<double, double> position,
+		std::string asset ,
+		std::string color ,
+		int maxVelocity,
+		int lifes ,
+		int availableBombs
+	);
+	void draw();
+	void update(double);
+	void keyPressedEvent(QKeyEvent* event);
+private:
+	int _lifes;
+	int _availableBombs;
+	std::string _color;
+	std::vector<double> _velocity;
+	double _maxVelocity;
+};
+
+#endif
